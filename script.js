5aa9b78f94e40ad23c755f73d75012b010907fd2
sokoli <sokoliabedin@gmail.com>
2023-11-16 01:13:11 +0100

    Abedin's Adding and code fixing js

--- script.js
+++ script.js
@@ -16,10 +16,10 @@ document.querySelector("#search-btn").onclick = () => {
 
 let cartItem = document.querySelector(".cart-items-container");
 
-document.quuerySelector("#cart-btn").onclick = () => {
-  cartItem.classList.toggle("active");
-  navbar.classList.remove("active");
-  searchForm.classList.remove("active");
+document.querySelector('#cart-btn').onclick = () => {
+  cartItem.classList.toggle('active');
+  navbar.classList.remove('active');
+  searchForm.classList.remove('active');
 };
 
 window.onscroll = () => {
